e03ce654207f2fbea06d396605a3a7eab7e75ec7
Vedant Agarwala <vedant.kota@gmail.com>
2015-10-18 20:09:34 +0530

    Modified SQL for single address

--- app/src/main/java/vedant/myspending/app/SmsReaderSingleton.java
+++ app/src/main/java/vedant/myspending/app/SmsReaderSingleton.java
@@ -15,31 +15,38 @@ import vedant.myspending.app.models.Sms;
  */
 public class SmsReaderSingleton {
     private static final String TAG = "SmsReaderSingleton";
-    private static final String FROM_CITIBANK = "VM-Citibk";
+    private static final String[] COLUMN_NAMES = {"_id", "address",  "date", "body"};
+
+    private static final String FROM_CITIBANK = "citibk";
     private static SmsReaderSingleton ourInstance = new SmsReaderSingleton();
 
     public Observable<Sms> fetchSmses(Context context) {
-        Cursor cursor = context.getContentResolver().query(Uri.parse("content://sms/inbox"),
-                COLUMN_NAMES,
-                null, null, null);
-
-        if (cursor != null && cursor.moveToFirst()) { // must check the result to prevent exception
-            do {
-                Log.d(TAG, String.valueOf(parseSms(cursor)));
+        return Observable.<Sms>create(subscriber -> {
+            Cursor cursor = context.getContentResolver().query(Uri.parse("content://sms/inbox"),
+                    COLUMN_NAMES, // select
+                    String.format("LOWER(%s) LIKE LOWER(?) ", COLUMN_NAMES[1]), // where column address contains ?
+                    new String[] {"%" + FROM_CITIBANK + "%"},
+                    COLUMN_NAMES[2] + " DESC");
+            if (cursor != null && cursor.moveToFirst()) { // must check the result to prevent exception
+                do {
+                    Sms sms = parseSms(cursor);
+                    Log.d(TAG, String.valueOf(sms));
+                    if (!subscriber.isUnsubscribed())
+                        subscriber.onNext(sms);
 //                cursor.getCol
-            } while (cursor.moveToNext());
-            cursor.close();
-        } else {
-            // empty box, no SMS
-        }
-        return null;
+                } while (cursor.moveToNext());
+                cursor.close();
+            } else {
+                // empty box, no SMS
+                Log.e(TAG, "no matching smses");
+            }
+        });
+//                .subscribeOn(Schedulers.io());
     }
 
     public static SmsReaderSingleton getInstance() {
         return ourInstance;
     }
-
-    private static final String[] COLUMN_NAMES = {"_id", "address",  "date", "body"};
 
     private Sms parseSms(Cursor cursor) {
         return new Sms(
